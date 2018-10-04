58ded455724be55d864984a932430cd4c858268f
danshook <shook.dan@gmail.com>
2018-10-04 14:47:14 -0700

    opening and closing side drawer

--- my-page/src/App.js
+++ my-page/src/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 
 import Toolbar from './components/Toolbar/Toolbar';
 import SideDrawer from './components/SideDrawer/SideDrawer';
-import Bacdrop from './components/Backdrop/Backdrop';
+import Backdrop from './components/Backdrop/Backdrop';
 
 class App extends Component {
+  state = {
+    sideDrawerOpen: false
+  };
+
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen};
+    });
+  };
+
+
   render() {
+    let sideDrawer;
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      sideDrawer = <SideDrawer />
+      backdrop = <Backdrop />
+    }
     return (
       <div style={{height: '100%'}}>
-       <Toolbar />
-       <SideDrawer />
-       <Bacdrop />
+       <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
+       {sideDrawer}
+       {backdrop}
+       
         <main style={{marginTop: '64px'}}>  
           <p>This is the page content!</p>
         </main>
